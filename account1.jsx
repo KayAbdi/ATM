17e2f3cd6b072bf92e4b17deac9e4edfee69c6b4
Kay abdi <abdifiiq@gmail.com>
2022-03-07 15:25:21 -0600

    added some styling and more functionalities

--- account1.jsx
+++ account1.jsx
@@ -1,32 +1,85 @@
-const ATMDeposit = ({ onChange }) => {
+const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
+    const choice = ['Deposit', 'Withdraw'];
+    console.log(`ATM isDeposit: ${isDeposit}`);
     return (
-        <label className="label huge">
-            Deposit: 
-            <input type="number" onChange={onChange}></input>
-            <input type="submit" value="Submit"></input>
-        </label>
+      <label className="label huge">
+        <h3> {choice[Number(!isDeposit)]}</h3>
+        <input id="number-input" type="number" width="200" onChange={onChange}></input>
+        <input type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input"></input>
+      </label>
     );
-};
-
-const Account = () => {
-    let transactionState = 0;
-    let totalState = 0;
-    let status = "Account Balance is $zero";
-    const handleChange = event => {
-        console.log(`handleChange ${event.target.value}`);
-        transactionState = Number(event.target.value);
+  };
+  
+  const Account = () => {
+    // let deposit = 0; // state of this transaction
+    const [deposit, setDeposit] = React.useState(0);
+    const [totalState, setTotalState] = React.useState(0);
+    const [isDeposit, setIsDeposit] = React.useState(true);
+    const [atmMode, setAtmMode] = React.useState('');
+    const [validTransaction, setValidTransaction] = React.useState(false);
+  
+    let status = `Account Balance $ ${totalState} `;
+    console.log(`Account Rendered with isDeposit: ${isDeposit}`);
+    const handleChange = (event) => {
+      console.log(Number(event.target.value));
+      if (Number(event.target.value) <= 0) {
+        return setValidTransaction(false);
+      }
+      if (atmMode === 'Withdraw' && Number(event.target.value) > totalState) {
+        setValidTransaction(false);
+      } else {
+        setValidTransaction(true);
+      }
+      setDeposit(Number(event.target.value));
     };
-    const handleSubmit = event => {
-        totalState += transactionState;
-        status = `Account Balance is $ ${totalState}`;
-        document.getElementById("total").innerHTML = status;
-        event.preventDefault();
+    const handleSubmit = (event) => {
+      let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+      setTotalState(newTotal);
+      setValidTransaction(false);
+      event.preventDefault();
     };
+  
+    const handleModeSelect = (event) => {
+      console.log(event.target.value);
+      setAtmMode(event.target.value);
+      setValidTransaction(false);
+      if (event.target.value === 'Deposit') {
+        setIsDeposit(true);
+      } else {
+        setIsDeposit(false);
+      }
+    };
+  
     return (
-        <form onSubmit={handleSubmit}>
-            <h2 id="total">{status}</h2>
-            <ATMDeposit onChange={handleChange}>Deposit</ATMDeposit>
-        </form>
+        <div id="divy" className="card text-white bg-primary mb-3">
+            <div className="card-header">YOUR BANK</div>
+                <div class="card-body">
+                    <form onSubmit={handleSubmit}>
+                        <>
+                        <h2 id="total">{status}</h2>
+                        <label>Select an action below to continue</label>
+                        <select onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
+                            <option id="no-selection" value=""></option>
+                            <option id="deposit-selection" value="Deposit">
+                            Deposit
+                            </option>
+                            <option id="withdraw-selection" value="Withdraw">
+                            Withdraw
+                            </option>
+                        </select>
+                        {atmMode && (
+                            <ATMDeposit
+                            onChange={handleChange}
+                            isDeposit={isDeposit}
+                            isValid={validTransaction}
+                            ></ATMDeposit>
+                        )}
+                        </>
+                    </form>
+                </div>
+            </div>
     );
-};
-ReactDOM.render(<Account/>,document.getElementById("root"));
+  };
+  // ========================================
+  ReactDOM.render(<Account />, document.getElementById('root'));
+  
